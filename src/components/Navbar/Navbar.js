1f757bec1e859f930b54c02fd795f8307f1781e0
Gaurav Chaudhary <gauravchaudhary3000@gmail.com>
2022-08-24 17:25:21 +0530

    Added github link

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,14 +3,14 @@ import './Navbar.css'
 const Navbar = () => {
   return (
     <nav>
-    <ul>
-        <li className='title'>
-        Trello
+      <ul>
+        <li className="title">Trello</li>
+        <li className="github">
+          <a href="https://github.com/mrgauravchaudhary/Trello-App">
+            Github Link
+          </a>
         </li>
-        <li className='github'>
-        <a href="/">Github Link</a>
-        </li>
-    </ul>
+      </ul>
     </nav>
   );
 }
